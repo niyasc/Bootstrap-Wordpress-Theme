08008c5ee1fc3534ac76969930c4c5a07c54876f
Niyas C <niyasc@localhost.localdomain>
2014-10-19 21:08:49 +0530

    Almost fixed problem with slide show

--- slideshow.php
+++ slideshow.php
@@ -1,23 +1,22 @@
-<div id="carousel-example-generic" class="carousel slide container" data-ride="carousel" data-interval="2000">
+<div id="carousel-example-generic" class="carousel slide image-responsive" data-ride="carousel" data-interval="2000">
 <!-- Carousel indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-		<li data-target="#myCarousel" data-slide-to="3"></li>
-		<li data-target="#myCarousel" data-slide-to="4"></li>
-	</ol>   
+<ol class="carousel-indicators">
+<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+<li data-target="#myCarousel" data-slide-to="1"></li>
+<li data-target="#myCarousel" data-slide-to="2"></li>
+<li data-target="#myCarousel" data-slide-to="3"></li>
+<li data-target="#myCarousel" data-slide-to="4"></li>
+</ol>
 	<div class="section-title">
-			Featured
-		</div>
+		Featured
+	</div>
 	<!-- Carousel items -->
 	<div class="carousel-inner">
-		
-		<?php 
+		<?php
 		$looped = new WP_Query(array('post_type' => 'post', 'posts_per_page' => 5 ));
 		$c = 0;
-		if ( $looped->have_posts() ) { 
-			while ( $looped->have_posts() ) { 
+		if ( $looped->have_posts() ) {
+			while ( $looped->have_posts() ) {
 				$looped->the_post();
 				$c += 1;
 				if (has_post_thumbnail()) {
@@ -26,37 +25,29 @@
 				else {
 					$url = array("wp-content/themes/TechnoRevolution3/bootstrap/img/noimg.png");
 				}
-		?>
-				<?php if($c ==  1) :?>
-						<?php echo "<div class=\"item active\" style=\"background-image: url('".$url[0]."'); >"; ?>
+				?>
+				<?php if($c == 1) :?>
+					<div class="item active">
 				<?php else : ?>
-						<!--<div class="item">-->
-						<?php echo "<div class=\"item\" style=\"background-image: url('".$url[0]."'); >"; ?>
+					<div class="item">
 				<?php endif; ?>
-				<a href="<?php echo get_permalink();?>"> 	
-					<!--<img src="<?=$url[0]?>">-->
-					<a href="<?php the_permalink() ?>">
-					<div class="carousel-caption">
-        				<h2><?php the_title(); ?></h2>
-      				</div>
-      				</a>	
-				</a>
-
-            </div>
-				<?php
-			}//end of while
-		}//end of if
-		wp_reset_postdata(); ?>
+					<a href="<?php echo get_permalink();?>">
+						<img src="<?=$url[0]?>" style="margin: auto;">
+							<div class="carousel-caption">
+								<h3><?php the_title(); ?></h3>
+							</div>
+					</a>
+				</div>
+			<?php
+		}//end of while
+	}//end of if
+	wp_reset_postdata(); ?>
 	</div>
-
-        <!-- Carousel nav -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-
+	<!-- Carousel nav -->
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+	</a>
 </div>
-
-

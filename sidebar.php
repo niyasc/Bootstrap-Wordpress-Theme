5b15cf51e4ff3e9892bdb4795830c2c3b5893817
Niyas C <niyasc@localhost.localdomain>
2014-10-20 14:46:01 +0530

    Social media link added

--- sidebar.php
+++ sidebar.php
@@ -2,10 +2,10 @@
 	<div class="social-bg">
 		<div class="social-icon-container">
 			<div class="follow-text">Follow Us</div>
-			<div class="facebook-icon"></div>
-			<div class="googleplus-icon"></div>
-			<div class="twitter-icon"></div>
-			<div class="rss-icon"></div>
+			<a href="http://facebook.com/technorevolute" target="_blank"><div class="facebook-icon"></div></a>
+			<a href="http://plus.google.com/+TechnorevolutionIn" target="_blank"><div class="googleplus-icon"></div></a>
+			<a href="http://twitter.com/technorevolute" target="_blank"><div class="twitter-icon"></div></a>
+			<a href="http://technorevolution.in/main/feed" target="_blank"><div class="rss-icon"></div></a>
 		</div>
 	</div>
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-1') ) : ?>
